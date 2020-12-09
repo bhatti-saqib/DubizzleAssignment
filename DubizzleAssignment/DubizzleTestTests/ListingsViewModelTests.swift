b05bc5f6a6e077be3ddb8d83207cb90f4c1abd1f
saqibsohailbhatti <saqibbhatti12@hotmail.com>
2020-12-09 12:08:26 +0500

    Latest changes

---
+++ DubizzleAssignment/DubizzleTestTests/ListingsViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  ListingsViewModelTests.swift
+//  DubizzleTestTests
+//
+//  Created by Saqib Bhatti on 09/12/2020.
+//
+
+import XCTest
+@testable import DubizzleTest
+
+class ListingsViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
